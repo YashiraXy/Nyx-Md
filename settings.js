7fc7018e9b50a7bd6ebe6e6dcfd134597f1c5362
YashiraXy <106202932+YashiraXy@users.noreply.github.com>
2022-05-27 11:41:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ zenz: 'https://zenzapis.xyz',
 //—————「 Website Apikey 」—————//
 global.APIKeys = {
 //gausah di apa² in, udah free apikey :)
-'https://zenzapis.xyz': 'Yashi',
+'https://zenzapis.xyz': '1e0d50de15d1',
 }
 
 //—————「 Setting Owner 」—————//
